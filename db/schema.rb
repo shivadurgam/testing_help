0f725b8cf5f5b5d826a8c741ffb45346e554ce3a
Venkata Pasupuelti <subbarao.pasupuleti@gmail.com>
2015-03-27 08:09:34 -0400

    Removed devise models

--- db/schema.rb
+++ db/schema.rb
@@ -16,24 +16,6 @@ ActiveRecord::Schema.define(version: 20150326085357) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "models", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "models", ["email"], name: "index_models_on_email", unique: true, using: :btree
-  add_index "models", ["reset_password_token"], name: "index_models_on_reset_password_token", unique: true, using: :btree
-
   create_table "questions", force: :cascade do |t|
     t.string   "q1"
     t.string   "ans1"
